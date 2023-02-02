3ca9e310d661392c6d521f0ac70f0d2780a42e19
dfelsie <dfelsenthal88@gmail.com>
2023-02-02 17:18:56 -0500

    tailwind still being annoying

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import Content from "../components/Content/Content";
 import FooterLinks from "../components/FooterLinks/FooterLinks";
 import Header from "../components/Header/Header";
